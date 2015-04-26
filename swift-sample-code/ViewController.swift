5d35d12dfaca89728b7850ae2495b763efc96661
Yohei Munesada <y.munesada@gmail.com>
2015-04-26 22:52:43 +0900

    add navigationController

--- swift-sample-code/ViewController.swift
+++ swift-sample-code/ViewController.swift
@@ -3,23 +3,50 @@
 //  swift-sample-code
 //
 //  Created by Munesada Yohei on 2015/04/26.
-//  Copyright (c) 2015年 Munesada Yohei. All rights reserved.
+//  Copyright (c) 2015 Munesada Yohei. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tableView = UITableView()
 
+    
+//MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.addUIComponents()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+
+//MARK : UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel?.text = String(indexPath.row)
+        return cell
+    }
+
+
+//MARK : Private Functions
+    
+    /**
+        UIコンポーネントをViewに配置する
+    */
+    private func addUIComponents() {
+        
+        tableView.frame = self.view.frame
+        tableView.dataSource = self
+        tableView.delegate = self
+        self.view.addSubview(tableView)
+    }
 
 }
 
